9546c41cdf11295ab31b2e509d446d6b1bc81cab
Bragio06 <142274731+Bragio06@users.noreply.github.com>
2025-08-28 08:42:26 -0300

    Atividade da aula

---
+++ atividade2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class ReceberDados
+{
+    public static void Main(string[] args)
+    {
+        int resposta;
+        double celsius, fahrenheit, kelvin, rankine;
+
+        Console.WriteLine("Digite qual temperatura voce quer converter?");
+        Console.WriteLine("Digite 1 para celsius ; 2 para fahrenheit; 3 para kelvin; 4 para rankine:");
+        resposta = Convert.ToInt32(Console.ReadLine());
+        if (resposta == 1)
+        {
+            Console.WriteLine("Digite a teperatura em celsius:");
+            celsius = Convert.ToDouble(Console.ReadLine());
+            rankine = (celsius * 9 / 5) + 491.67;
+            fahrenheit = (celsius * 9 / 5) + 32;
+            kelvin = celsius + 273.15;
+            Console.WriteLine("kelvin:" + kelvin);
+            Console.WriteLine("rankine:" + rankine);
+            Console.WriteLine("fahrenheit:" + fahrenheit);
+        }
+        else if (resposta == 2)
+        {
+            Console.WriteLine("Digite a teperatura em fahrenheit:");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            rankine = fahrenheit + 459.67;
+            celsius = (fahrenheit - 32) * 5 / 9;
+            kelvin = (fahrenheit - 32) * 5 / 9 + 273.15;
+
+            Console.WriteLine("kelvin:" + kelvin);
+            Console.WriteLine("rankine:" + rankine);
+            Console.WriteLine("celsius:" + celsius);
+        }
+        else if (resposta == 3)
+        {
+            Console.WriteLine("Digite a teperatura em kelvin:");
+            kelvin = Convert.ToDouble(Console.ReadLine());
+
+            rankine = kelvin * 1.8;
+            celsius = kelvin - 273.15;
+            fahrenheit = (kelvin - 273.15) * 9 / 5 + 32;
+
+            Console.WriteLine("celsius:" + celsius);
+            Console.WriteLine("rankine:" + rankine);
+            Console.WriteLine("fahrenheit:" + fahrenheit);
+        }
+        else
+        {
+            Console.WriteLine("Digite a teperatura em rankine:");
+            rankine = Convert.ToDouble(Console.ReadLine());
+
+            kelvin =rankine * 5/9;
+            celsius = (rankine - 491.67) * 5/9 ;
+            fahrenheit = rankine - 459.67;
+
+            Console.WriteLine("kelvin:" + kelvin);
+            Console.WriteLine("celsius:" + celsius);
+            Console.WriteLine("fahrenheit:" + fahrenheit);
+        }
+
+
+
+       
+
+    }
+}
+
+
