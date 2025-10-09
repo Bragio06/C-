f9dc84f0bb7125a100e3b6268dcb8bd43d306e87
Bragio06 <142274731+Bragio06@users.noreply.github.com>
2025-10-09 10:59:22 -0300

    termino

---
+++ arquivo/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+public class ARQUIVO
+{
+    FileStream arq = new FileStream("dados.txt", fileMode.Open, FileAccess.Read);
+    StreamReader sr = new StreamReader(arq, Encoding.UTF8);
+
+    string str;
+    int soma = 0;
+    while (!string.EndOfStream)
+    {
+       str = sr.ReadLine();
+    }
+}
